96d368b1af5bc0ea4d7a8c2034cd3b36d219716c
Pietro Delfino <delfino.pietro@gmail.com>
2018-04-20 15:42:00 +0200

    added CV

--- build_site.R
+++ build_site.R
@@ -1,7 +1,7 @@
 #Set our working directory. 
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
-#working on at the time. 
+#working on at the time.
 setwd("/home/dianalab/DATA/BIOINFORMATICS/Dropbox/PETER/GITHUB/my_website/pietrod.github.io/")
 
 #render your sweet site. 
