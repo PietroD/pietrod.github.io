7c8061cdf8cc0ebe2dab01f7a87867f517aa0d77
pietrod <delfino.pietro@gmail.com>
2019-04-30 09:25:51 +0200

    Updated publications

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time.
-setwd("/home/dianalab/DATA/BIOINFORMATICS/Dropbox/PETER/GITHUB/my_website/pietrod.github.io/")
+setwd("~/OneDrive - Università degli Studi di Verona/PETER/VARI/various/GITHUB/my_website/pietrod.github.io/")
 
 #render your sweet site. 
 rmarkdown::render_site()
